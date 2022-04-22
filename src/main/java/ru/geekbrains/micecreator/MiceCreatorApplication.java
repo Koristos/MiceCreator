b00be12c3fbd932cf45f01073d4bfe43b13f9b1f
fefelov.sergei <fefelov.sergei@otr.ru>
2022-04-23 00:13:46 +0300

    Initial commit

---
+++ src/main/java/ru/geekbrains/micecreator/MiceCreatorApplication.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.micecreator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiceCreatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiceCreatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/geekbrains/micecreator/MiceCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.micecreator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiceCreatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
