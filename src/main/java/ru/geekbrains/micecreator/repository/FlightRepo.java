b1ba9776b5138126dac7f3d4bd0631d198ccd90a
fefelov.sergei <fefelov.sergei@otr.ru>
2022-05-18 21:47:16 +0300

    LocalDateTime Fix

--- src/main/java/ru/geekbrains/micecreator/repository/FlightRepo.java
+++ src/main/java/ru/geekbrains/micecreator/repository/FlightRepo.java
@@ -3,14 +3,14 @@ package ru.geekbrains.micecreator.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.geekbrains.micecreator.models.complex.Flight;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface FlightRepo extends JpaRepository<Flight, Integer> {
 
 	List<Flight> findByTourId(Integer tourId);
 
-	List<Flight> findByDepartureAirportIdAndArrivalAirportIdAndDepartureDateBetween(Integer departureAirportId, Integer arrivalAirportId, LocalDate firstDate, LocalDate secondDate);
+	List<Flight> findByDepartureAirportIdAndArrivalAirportIdAndDepartureDateBetween(Integer departureAirportId, Integer arrivalAirportId, LocalDateTime firstDate, LocalDateTime secondDate);
 
-	List<Flight> findByDepartureAirportIdAndArrivalAirportIdAndAirlineIdAndDepartureDateBetween(Integer departureAirportId, Integer arrivalAirportId, Integer airlineId, LocalDate firstDate, LocalDate secondDate);
+	List<Flight> findByDepartureAirportIdAndArrivalAirportIdAndAirlineIdAndDepartureDateBetween(Integer departureAirportId, Integer arrivalAirportId, Integer airlineId, LocalDateTime firstDate, LocalDateTime secondDate);
 }
